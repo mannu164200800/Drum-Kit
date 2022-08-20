b102affc9940359d8e9551fa83c894b1cbd69d1a
mannu1642008 <97331725+mannu1642008000@users.noreply.github.com>
2022-08-20 21:38:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+ let promo =   prompt('What,s your good name');
+      alert("Hello!" + " " +  promo + " " + 'Welcome to this amazing drum kit & play drum ');
+     window.addEventListener('keydown',function(e) {
+       const audio  = document.querySelector(`audio[data-key="${e.keyCode}"`);
+       const key = document.querySelector(`.key[ data-key="${e.keyCode}"`);
+
+       if(!audio) return //stops the function from running all together//
+        audio.currentTime = 0; //rewind to start 
+      audio.play();
+       console.log(key);
+       key.classList.add('playing');
+
+     });
+      function removeTransition(e) {
+        if(e.propertyName !== 'transform') return //skip it if it's not a transform
+          this.classList.remove('playing');
+       }
+
+
+     const keys = document.querySelectorAll('.key');
+     keys.forEach(key=>key.addEventListener('transitionend',removeTransition));
